9f25145ffa66ae1d6911fda2399c1079055ef9ca
Abd Moh <abdpuni6pusl9@gmail.com>
2023-09-14 14:27:32 +0300

    edited throttle

--- routes/api.php
+++ routes/api.php
@@ -11,14 +11,14 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/send-register-otp',[OTPController::class,'sendRegisterOTP'])->middleware(['throttle:1,3']);
+    Route::get('/send-register-otp',[OTPController::class,'sendRegisterOTP'])->middleware(['throttle:2,1']);
     Route::post('/verify-register-otp', [OTPController::class,'verifyRegisterOTP'])->middleware(['signed','throttle:3,1'])->name('verification.otp');
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [UserController::class, 'profile']);
     Route::post('/upload-profile-image', [UserController::class, 'uploadProfileImage']);
 });
 
-Route::post('/send-reset-otp',[OTPController::class,'sendResetOTP'])->middleware('throttle:1,2');
+Route::post('/send-reset-otp',[OTPController::class,'sendResetOTP'])->middleware('throttle:2,1');
 Route::post('/verify-reset-otp',[OTPController::class,'verifyResetOTP'])->middleware('throttle:3,1');
 Route::post('/reset-password',[OTPController::class,'resetPassword']);
 
