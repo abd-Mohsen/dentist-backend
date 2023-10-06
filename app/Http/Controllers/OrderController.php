f0788dc37b1d0b177c3864e5f63e0626663addf7
Abd Moh <abdpuni6pusl9@gmail.com>
2023-10-06 22:53:56 +0300

    implemented "create order"

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use App\Models\Product;
+use App\Models\SubOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\OrderResource;
 
 class OrderController extends Controller
 {
@@ -23,13 +26,51 @@ class OrderController extends Controller
     public function store(Request $request) //make a new order
     {
         $this->authorize('create', Order::class);
+        $customer_id = $request->user()->id;
 
         $data = $request->validate([
-            //hashmap of products and quantities
+            '*.product_id' => 'required|integer',
+            '*.quantity' => 'required|integer',
+            '*.price' => 'required|double',
         ]);
 
-        // create order
-        // create suborders and pivots
+        $order = null;
+
+        DB::transaction(function () use ($data, $customer_id) {
+            // Create the order
+            $order = Order::create([
+                'customer_id' => $customer_id,
+            ]);
+        
+            // Group the products by supplier
+            $products_by_supplier = collect($data)
+                ->groupBy(function ($item) {
+                    $product = Product::findOrFail($item['product_id']);
+                    return $product->supplier_id;
+                });
+        
+            // Create a suborder for each group of products from the same supplier
+            foreach ($products_by_supplier as $supplier_id => $products) {
+                $suborder = Suborder::create([
+                    'order_id' => $order->id,
+                    'supplier_id' => $supplier_id,
+                    'status' => 'pending',
+                ]);
+        
+                // Add the products to the suborder
+                foreach ($products as $product) {
+                    $suborder->products()->attach($product['id'], [
+                        'quantity' => $product['quantity'],
+                        'price' => $product['price'],
+                    ]);
+                }
+            }
+        });
+        
+        return response()->json([
+            'message' => 'order has been submitted',
+            'order' => new OrderResource($order),
+        ], 201);
     }
 
 
